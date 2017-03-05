98360e903549ea4438c53d00661ad57b36d3efc0
Maxime Soulie <maxime.soulie@gmail.com>
2017-03-05 04:27:32 +0000

    Added contact create action to save to db et typo dans form_for new.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130004401) do
+ActiveRecord::Schema.define(version: 20170305032407) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
